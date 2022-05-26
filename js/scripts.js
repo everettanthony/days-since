77d2fa9854212aba080b4941a73bd41c2264f76d
everettanthony <mrtonybradshaw@gmail.com>
2022-05-26 11:23:52 -0500

    First push

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+  daysSince('May 24, 2022 00:00:00'); 
+};
+
+function daysSince(countFrom) {
+
+  let now = new Date(),
+      startDay = new Date(countFrom),
+      timeDifference = (now - startDay);  
+  let secondsInADay = 60 * 60 * 1000 * 24;
+    
+  days = Math.floor(timeDifference / (secondsInADay) * 1);
+
+  const count = document.querySelector('.count');
+  count.innerHTML = days;
+
+  clearTimeout(daysSince.interval);
+  daysSince.interval = setTimeout(function(){ daysSince(countFrom); }, 1000);
+}
